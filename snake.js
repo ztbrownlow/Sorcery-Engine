a65bc071067ce491f4d11cd2f087a2f6e473e0ea
ztbrownl <ztbrownl@ncsu.edu>
2017-09-17 19:44:17 -0400

    poison food spawns regular food too

---
+++ snake.js
@@ -0,0 +1,174 @@
+var game = new Game(document.getElementById("canvas"));
+var snakeSize = 20;
+
+var spr_snake_head = game.sprites.push(new FilledRect("snake_head", snakeSize, snakeSize, "#008800"));
+var spr_snake_body = game.sprites.push(new FilledRect("snake_body", snakeSize, snakeSize, "#008800"));
+var spr_snake_tail = game.sprites.push(new FilledRect("snake_tail", snakeSize, snakeSize, "#008800"));
+var spr_food = game.sprites.push(new FilledRect("food", snakeSize, snakeSize, "#FFFF00"));
+var spr_food_rotten = game.sprites.push(new FilledRect("food_rotten", snakeSize, snakeSize, "#888800"));
+var spr_wall = game.sprites.push(new FilledRect("wall", snakeSize, snakeSize, "#000000"));
+
+var obj_snake_tree = game.objects.push(new SceneGraph("snake", true, true, false));
+var obj_food_tree = game.objects.push(new SceneGraph("food", true, true, false));
+var obj_wall_tree = game.objects.push(new SceneGraph("wall", true, true, false));
+
+game.lose = function() {
+  console.log("Game lost");
+  //TODO highscore stuff
+  game.setup();
+}
+
+var head;
+
+game.setup = function() {
+  game.score = 0;
+  obj_snake_tree.removeAll();
+  obj_food_tree.removeAll();
+  head = obj_snake_tree.push(new Head(spr_snake_head, spr_snake_body, spr_snake_tail, snakeSize, obj_snake_tree));
+  obj_snake_tree.push(new Body(spr_snake_tail, head));
+  obj_food_tree.push(new Food());
+  //other stuff probably
+}
+
+//move to engine for refactoring for next week
+game.outOfBounds = function(x, y) {
+  return x > game.canvas.width || x < 0 || y > game.canvas.height || y < 0;
+}
+
+var score = 0;
+
+function Head(sprite, body_sprite, tail_sprite, snakeSize, tree) {
+  var self = this;
+  self.constructor = function(sprite, snakeSize, tree, body_sprite, tail_sprite) {
+    GameObject.call(self, "snake_head", sprite, snakeSize, snakeSize)
+    self.sprite = sprite;
+    self.snakeSize = snakeSize;
+    self.lastX = 0;
+    self.lastY = snakeSize;
+    self.direction = [1, 0];
+    self.tree = tree;
+    self.body_sprite = body_sprite;
+    self.tail_sprite = tail_sprite;
+  }
+  self.constructor(sprite, snakeSize, tree, body_sprite, tail_sprite);
+  self.update = function(game) {
+    self.lastX = self.x;
+    self.lastY = self.y;
+    self.x += self.direction[0] * snakeSize;
+    self.y += self.direction[1] * snakeSize;
+    if (game.outOfBounds(self.x, self.y)) {
+      game.lose();
+    } else {
+      game.objects.forEachUntilFirstSuccess( function(e) {return self.tryCollide(e); }, true);
+    }
+  }
+  self.canCollideWith = function(other) { 
+    return true;
+  }
+  self.collideWith = function(other) {
+    if (other instanceof Food) {
+      if (other.rotten) {
+        --game.score;
+        if (self.tree.length == 1) {
+          game.lose();
+        } else {
+          self.tree.pop();
+          var last = self.tree.last();
+          if (last != self) {
+            last.sprite = self.tail_sprite;
+          }
+        }
+        obj_food_tree.remove(other);
+      } else {
+        ++game.score;
+        var last = self.tree.last();
+        last.sprite = self.body_sprite;
+        self.tree.push(new Body(self.tail_sprite, last));
+        other.placeInGame(); //place food again
+      }
+    } else {
+      game.lose();
+    }
+  }
+}
+
+function Body(sprite, follow) {
+  var self = this;
+  self.constructor = function(sprite, follow) {
+    GameObject.call(self, "body", sprite, follow.x, follow.y); //I would expect this to use lastX/lastY but it doesn't so yeah
+    self.follow = follow;
+    self.lastX = self.x;
+    self.lastY = self.y;
+  }
+  self.constructor(sprite, follow);
+  self.update = function(game) {
+    self.lastX = self.x;
+    self.lastY = self.y;
+    self.x = self.follow.lastX;
+    self.y = self.follow.lastY;
+  }
+}
+
+function Food() {
+  var self = this;
+  self.placeInGame = function() {
+    var x;
+    var y;
+    var temp;
+    do {
+      x = Math.floor(Math.random() * game.canvas.width / snakeSize) * snakeSize;
+      y = Math.floor(Math.random() * game.canvas.height / snakeSize) * snakeSize;
+      temp = game.objects.pointCollide(x, y, false);
+    } while (!temp || flatten(temp).filter(function(e) {return e;}).length != 0);
+    self.x = x;
+    self.y = y;
+    self.steps = 0;
+    self.rotten = false;
+    self.sprite = self.mainSprite;
+  }
+  self.constructor = function() {
+    self.placeInGame();
+    GameObject.call(self, "food", spr_food, self.x, self.y);
+    self.mainSprite = self.sprite;
+    self.stepsUntilRotten = 30;
+    self.rottenSprite = spr_food_rotten;
+  }
+  self.constructor();
+  self.update = function(game) {
+    self.steps += 1;
+    if (self.steps == self.stepsUntilRotten) {
+      self.sprite = self.rottenSprite;
+      self.rotten = true;
+      obj_food_tree.push(new Food());
+    }
+  }
+}
+
+window.addEventListener("keydown", onKeyDown, false); //I want this to be game.canvas.addEventListener but that doesn't seem to work
+//probably also want a key up event at some point but don't need it right now
+//may want to eventually refactor this to check at the start of an update instead of being asynchronous?
+// ^ could handle this using a queue of events and run through them at the start of update function
+function onKeyDown(event) {
+  var keyCode = event.keyCode;
+  switch (keyCode) {
+    case 68: //d
+      if (head.direction[0] != -1)
+        head.direction = [1, 0]
+      break;
+    case 83: //s
+      if (head.direction[1] != -1)
+        head.direction = [0, 1]
+      break;
+    case 65: //a
+      if (head.direction[0] != 1)
+        head.direction = [-1, 0]
+      break;
+    case 87: //w
+      if (head.direction[1] != 1)
+        head.direction = [0, -1]
+      break;
+  }
+}
+
+game.setup();
+game.start(100);
