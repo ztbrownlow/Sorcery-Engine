f178a7930efaa94b24ae8733139c2252cfaa6209
ztbrownl <ztbrownl@ncsu.edu>
2017-11-30 17:39:24 -0500

    documentation for engine.js

--- engine.js
+++ engine.js
@@ -7,10 +7,19 @@ document.write('<script type="text/javascript" src="gameManager.js"></script>');
 document.write('<script type="text/javascript" src="Grid.js"></script>');
 
 /**
- * Flattens a array of arrays (of arrays...) into a single array
+ * Flattens an n-dimensional array
+ * @param {Array} arrays array to flatten
+ * @param {Boolean} [removeNullsAndUndefineds=true] whether or not to remove null and undefined values when flattening the array. Defaults to true.
+ * @return flattened array
  */
-function flatten(arrays) {
-  return arrays.reduce(function(a, b){ if(a){return a.concat(b)} else {return b} });
+function flatten(arrays, removeNullsAndUndefineds=true) {
+  return arrays.reduce(
+    function(a,b) {
+      if (removeNullsAndUndefineds && (b == null || b == undefined)) {
+        return a;
+      }
+      return a.concat(Array.isArray(b) ? flatten(b, removeNullsAndUndefineds) : b);
+    }, []);
 }
 
 /**
@@ -38,7 +47,7 @@ function Game(canvas, name) {
     self.timer = null;
     self.sprites = new SceneGraph("sprites");
     self.objects = new SceneGraph("objects");
-	self.score = new SceneGraph("score");
+    self.score = new SceneGraph("score");
     self.gameManager = new GameManager();
     canvas.addEventListener("mousemove", function(e) {self.mouseMove(e)});
     canvas.addEventListener("mousedown", function(e) {self.mouseDown(e)});
@@ -47,16 +56,27 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * Finds a random point that is not occupied by any part of any object
+    * @memberof Game
+    * @function Game#findRandomUnoccupiedPoint
+    * @param {SceneGraph} [objectTree=Game.objects] the objectTree to check against. If none, defaults to Game.objects.
+    * @param {Number} [step=1] how large of an increment to use when selecting a random point. If none, defaults to 1 pixel. Useful for grid-based games
+    * @return {Array} a random point that does not hit any objects in the format [x, y]
+    */
   self.findRandomUnoccupiedPoint = function(objectTree, step) {
     var x;
     var y;
     var temp;
+    if (!objectTree) {
+      objectTree = self.objects;
+    }
+    if (!step) {
+      step = 1;
+    }
     do {
       x = Math.floor(Math.random() * (self.canvas.width-1) / step) * step;
       y = Math.floor(Math.random() * (self.canvas.height-1) / step) * step;
-      temp = self.objects.pointCollide(x, y, false);
+      temp = objectTree.pointCollide(x, y, false);
     } while (!temp || flatten(temp).filter(function(e) {return e;}).length != 0);
     self.x = x;
     self.y = y;
@@ -64,17 +84,32 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * Checks if a coordinate is out of the bounds of the canvas
+    * @memberof Game
+    * @instance
+    * @function Game#outOfBounds
+    * @param {Number} x x coordinate to check
+    * @param {Number} y y coordinate to check
+    * @return {Boolean} true if coordinate is out of bounds
+    */
   self.outOfBounds = function(x, y) {
     return x >= self.canvas.width || x < 0 || y >= self.canvas.height || y < 0;
   }
   
   /**
-    *
-	*/
-  self.getObjectsUnderMouse = function() {
-    var temp = self.objects.pointCollide(self.mouseX, self.mouseY, true);
+    * Gets a list of all objects that are under the mouse
+    * @memberof Game
+    * @instance
+    * @function Game#getObjectsUnderMouse
+    * @param {SceneGraph} [objectTree=Game.objects] what objectTree to check through. Defaults to Game.objects
+    * @param {Boolean} [limitToClickable=true] whether or not to only check against clickable objects. Defaults to true.
+    * @return {Array} a list of objects under the mouse
+    */
+  self.getObjectsUnderMouse = function(objectTree=null, limitToClickable=true) {
+    if (!objectTree) {
+      objectTree = self.objects;
+    }
+    var temp = objectTree.pointCollide(self.mouseX, self.mouseY, limitToClickable);
     if (temp) {
       var f = flatten(temp);
       if (f)
@@ -84,8 +119,11 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * Mousedown function. Calls mouseDown on all of the clickable objects in the game
+    * @memberof Game
+    * @instance
+    * @function Game#mouseDown
+    */
   self.mouseDown = function(e) {
     var temp = self.getObjectsUnderMouse();
     if (temp.length > 0) {
@@ -94,8 +132,11 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * Mouseup function. Calls mouseUp on all of the clickable objects in the game
+    * @memberof Game
+    * @instance
+    * @function Game#mouseUp
+    */
   self.mouseUp = function(e) {
     var temp = self.getObjectsUnderMouse();
     if (temp.length > 0) {
@@ -104,23 +145,32 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * Mouseout function, called when the mouse leaves the canvas. By default, delgates to Game.MouseDown
+    * @memberof Game
+    * @instance
+    * @function Game#mouseOut
+    */
   self.mouseOut = function(e) {
     self.mouseUp(e);
   }
   
   /**
-    *
-	*/
+    * Mousemove function. Updates Game.mouseX and Game.mouseY
+    * @memberof Game
+    * @instance
+    * @function Game#mouseMove
+    */
   self.mouseMove = function(e) {
     self.mouseX = e.offsetX;
     self.mouseY = e.offsetY;
   }
   
   /**
-    *
-	*/
+    * Update function. In order, Gets key presses, runs all events from the game manager, updates all objects, updates all object states to reflect their new positions, and finally runs post-update events from the game manager.
+    * @memberof Game
+    * @instance
+    * @function Game#update
+    */
   self.update = function() {
     //self.handleMouseActions();
     Key.update();
@@ -131,20 +181,29 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * Draws the game by drawing all of the drawable objects
+    * @memberof Game
+    * @instance
+    * @function Game#draw
+    */
   self.draw = function() {
     self.objects.draw(self.context);
   }
   
   /**
-    *
-	*/
+    * User-defined custom pre draw function. By default, doesn't do anything. Override to use.
+    * @memberof Game
+    * @instance
+    * @function Game#customPreDraw
+    */
   self.customPreDraw = function(){}
   
   /**
-    *
-	*/
+    * Runs before draw call. Calls Game.customPreDraw
+    * @memberof Game
+    * @instance
+    * @function Game#preDraw
+    */
   self.preDraw = function() {
     if (self.customPreDraw) {
       self.customPreDraw();
@@ -152,54 +211,70 @@ function Game(canvas, name) {
   }
   
   /**
-    *
-	*/
+    * User-defined custom post draw function. By default, doesn't do anything. Override to use.
+    * @memberof Game
+    * @instance
+    * @function Game#customPostDraw
+    */
   self.customPostDraw = function(){}
   
   /**
-    *
-	*/
+    * Runs after draw call. Calls Game.customPostDraw and then draws the score if applicable
+    * @memberof Game
+    * @instance
+    * @function Game#postDraw
+    */
   self.postDraw = function() {
     if (self.customPostDraw) {
       self.customPostDraw();
     }
-	for(var i = 0; i < self.score.length; i++){
-		var currentScore = self.score.children[i];
-		if(currentScore.displayScore){
-			game.context.fillStyle = currentScore.scoreColor;
-			game.context.font = currentScore.scoreFont;
-			game.context.fillText("Score: " + currentScore.score, currentScore.scoreX, currentScore.scoreY);
-		}
-	}
+    for(var i = 0; i < self.score.length; i++){
+      var currentScore = self.score.children[i];
+      if(currentScore.displayScore){
+        game.context.fillStyle = currentScore.scoreColor;
+        game.context.font = currentScore.scoreFont;
+        game.context.fillText("Score: " + currentScore.score, currentScore.scoreX, currentScore.scoreY);
+      }
+    }
   }
   
   /**
-    *
-	*/
+    * Game loop. Runs through update and draw process
+    * @memberof Game
+    * @instance
+    * @function Game#loop
+    */
   self.loop = function() {
-    self.canvas.width = self.canvas.width;
     self.update();
+    self.canvas.width = self.canvas.width;
     self.preDraw();
     self.draw();
     self.postDraw();
   }
   
   /**
-    *
-	*/
+    * Starts running the game
+    * @memberof Game
+    * @instance
+    * @function Game#start
+    * @param {Number} milliseconds the amount of time that should be set to pass between game loop calls
+    */
   self.start = function(milliseconds) {
-    self.timer = setInterval(self.loop, milliseconds);
+    if (!self.timer) {
+      self.timer = setInterval(self.loop, milliseconds);
+    }
   }
   
   /**
-    *
-	*/
+    * Stops running the game
+    * @memberof Game
+    * @instance
+    * @function Game#stop
+    */
   self.stop = function() {
     clearInterval(self.timer);
+    self.timer = null;
   }
-  
-  /**
-    *
-	*/
+    
   self.constructor(canvas, name);
 }
