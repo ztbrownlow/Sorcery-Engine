8ac7897de7e65653aca02f238b2f59480482c11a
ztbrownl <ztbrownl@ncsu.edu>
2017-12-01 15:10:57 -0500

    scenegraph, objectpool documentation

--- SceneGraph.js
+++ SceneGraph.js
@@ -1,21 +1,33 @@
 /**
   * A SceneGraph holds all of the objects in the game
-  * @class
-  * @namespace SceneGraph
-  * @property {string} name - the name of the scenegraph
-  * @property {boolean} doUpdate - the value of if this scenegraph should update. Default value is true.
-  * @property {boolean} doDraw - the value of if objects in this scenegraph should be drawn. Default value is true.
-  * @property {boolean} isClickable - the value of if the objects in the scenegraph should be clickable. Default value is true.
-  * @property {array} children - the array that holds all the objects 
-  * @property {string} updateDirection - the direction that the objects will update in. Defaults to "forwards". This value can only be either "forwards" or "backwards".
-  * @property {string} drawDirection - the direction that the objects will be drawn. Defaults to "backwards". This value can only be either "forwards" or "backwards"
-  * @property {boolean} isSceneGraph - returns true if the object is a scenegraph. Default true.
+  * @class SceneGraph
+  * @param {String} name the name of the SceneGraph
+  * @param {Boolean} [doUpdate=true] whether or not the scene graph should perform updates
+  * @param {Boolean} [doDraw=true] whether or not the scene graph should do draw calls
+  * @param {Boolean} [clickable=true] whether or not the objects in this scene graph are clickable
+  * @param {String} [updateDirection="forwards"] which direction to parse tree in when updating. "forwards" or "backwards"
+  * @param {String} [drawDirection="backwards"] which direction to parse tree in when drawing. "forwards" or "backwards"
+  * @property {String} name - the name of the SceneGraph
+  * @property {Boolean} doUpdate - the value of if this SceneGraph should update.
+  * @property {Boolean} doDraw - the value of if objects in this SceneGraph should be drawn.
+  * @property {Boolean} isClickable - the value of if the objects in the SceneGraph should be clickable.
+  * @property {Array} children - the array that holds all the objects 
+  * @property {String} updateDirection - the direction that the tree will be navigated to update objects. This value can only be either "forwards" or "backwards".
+  * @property {String} drawDirection - the direction that the tree will be navigated to draw objects. This value can only be either "forwards" or "backwards"
+  * @property {Boolean} isSceneGraph=true - true to indicate the object is a SceneGraph
+  * @property {Boolean} _isAggregate=true - true to indicate the object represents an aggregate of other objects
   */
 function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDirection="forwards", drawDirection="backwards") {
   var self = this;
   /** Creates the SceneGraph objects
-    * @constructs SceneGraph
-	*/
+    * @function SceneGraph#constructor
+    * @param {String} name the name of the SceneGraph
+    * @param {Boolean} [doUpdate=true] whether or not the scene graph should perform updates
+    * @param {Boolean} [doDraw=true] whether or not the scene graph should do draw calls
+    * @param {Boolean} [clickable=true] whether or not the objects in this scene graph are clickable
+    * @param {String} [updateDirection="forwards"] which direction to parse tree in when updating. "forwards" or "backwards"
+    * @param {String} [drawDirection="backwards"] which direction to parse tree in when drawing. "forwards" or "backwards"
+    */
   self.constructor = function(name, doUpdate=true, doDraw=true, clickable=true, updateDirection="forwards", drawDirection="backwards") {
     self.name = name;
     self.doUpdate = doUpdate;
@@ -31,24 +43,40 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   self.constructor(name, doUpdate, doDraw, clickable, updateDirection, drawDirection);
   
   /** 
-    * @memberof SceneGraph
-	* @param {int} x - the x value
-	* @param {int} y - the y value
-	*/
+    * Checks for collisions at a certain point with objects contained in the scene graph. Will search through any SceneGraphs that are a child of this one as well.
+    * Please see the example for how output is handled. You may want to run output through {@link flatten}
+    * @function SceneGraph#pointCollide
+    * @param {Number} x - the x value to check
+    * @param {Number} y - the y value to check
+    * @param {Boolean} limitToClickable - whether or not to limit the search to clickable objects only
+    * @return {Array} n-dimensional list of which objects collide. 
+    * @example
+    * // sg is an already initalized SceneGraph
+    * // sg array representation == [objectA, objectB, [objectC, [objectD, objectE]]] (where inner arrays are other SceneGraphs)
+    * // Assume objectA and objectC don't collide at (0, 0), but the others do
+    * console.log(sg.pointCollide(0,0,false)); // [undefined, objectB, [undefined, [objectD, objectE]]]
+    * console.log(flatten(sg.pointCollide(0,0,false))); // [objectB, objectD, objectE]
+    */
   self.pointCollide = function(x, y, limitToClickable) {
     return self.forEachReturn(function(e){if (!limitToClickable || e.isClickable) { var temp = e.pointCollide(x, y); if (temp) return e.isSceneGraph ? temp : e}});
   }
   
   /** Returns true if the SceneGraph is empty and contains no objects
-    * @memberof SceneGraph
-	*/
+    * @function SceneGraph#isEmpty
+    * @returns true if SceneGraph is empty
+    */
   self.isEmpty = function() {
     return (self.children.length == 0);
   }
   
   /**
-    *
-	*/
+    * handles mouseDown by passing event along to children if clickable
+    * @function SceneGraph#mouseDown
+    * @param {Game} game instance of the game
+    * @param {MouseEvent} event instance of the mouse event
+    * @param {Boolean} [returnOnFirstSuccess=true] whether or not to stop once we've found one object that is clicked
+    * @returns {object|object[]} output of mouseDown. Will likely be undefined
+    */
   self.mouseDown = function(game, event, returnOnFirstSuccess = true) {
     if (isClickable) {
       if (returnOnFirstSuccess) {
@@ -60,8 +88,13 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * handles mouseUp by passing event along to children if clickable
+    * @function SceneGraph#mouseUp
+    * @param {Game} game instance of the game
+    * @param {MouseEvent} event instance of the mouse event
+    * @param {Boolean} [returnOnFirstSuccess=true] whether or not to stop once we've found one object that is clicked
+    * @returns {object|object[]} output of mouseDown. Will likely be undefined
+    */
   self.mouseUp = function(game, event, returnOnFirstSuccess = true) {
     if (isClickable) {
       if (returnOnFirstSuccess) {
@@ -73,8 +106,10 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Handles updates by updating children
+    * @function SceneGraph#update
+    * @param {Game} game instance of game
+    */
   self.update = function(game) {
     if (doUpdate) {
       if (self.updateDirection == "backwards") {
@@ -86,15 +121,18 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Handles updates of positions by updating positions of children (new positions are stored and then updated after all other update stuff, e.g. collisions, is handled)
+    * @function SceneGraph#updatePosition
+    */
   self.updatePosition = function() {
     self.forEach(function(e) {e.updatePosition();});
   }
   
   /**
-    *
-	*/
+    * Handles draws by drawing children
+    * @function SceneGraph#draw
+    * @param {RenderingContext} context context to draw on
+    */
   self.draw = function(context) {
     if (self.doDraw) {
       if (self.drawDirection == "backwards") {
@@ -106,66 +144,89 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * pushes a new object onto the SceneGraph
+    * @function SceneGraph#push
+    * @param {object} object to add
+    * @returns {object} added object
+    */
   self.push = function(obj) {
     self.children.push(obj);
     return obj;
   }
   
   /**
-    *
-	*/
+    * pops a new object from the SceneGraph
+    * @function SceneGraph#pop
+    * @returns {object} popped object
+    */
   self.pop = function() {
     return self.children.pop();
   }
   
   /**
-    *
-	*/
+    * unshifts a new object to the SceneGraph (adds to start)
+    * @function SceneGraph#unshift
+    * @param {object} object to add
+    * @returns {object} added object
+    */
   self.unshift = function(obj) {
     self.children.unshift(obj);
     return obj;
   }
   
   /**
-    *
-	*/
-  self.shift = function(obj) {
+    * shifts a new object from the SceneGraph (removes from start)
+    * @function SceneGraph#shift
+    * @returns {object} unshifted object
+    */
+  self.shift = function() {
     return self.children.shift();
   }
   
   /**
-    *
-	*/
+    * Gets the first object in the SceneGraph
+    * @function SceneGraph#first
+    * @returns {object} first object
+    */
   self.first = function() {
     return self.children[0];
   }
   
   /**
-    *
-	*/
+    * Gets the last object in the SceneGraph
+    * @function SceneGraph#last
+    * @returns {object} last object
+    */
   self.last = function() {
     return self.children[self.children.length - 1];
   }
   
-  /**
-    *
-	*/
   Object.defineProperties(self, {
+    /**
+      * @memberof SceneGraph
+      * @instance
+      * @readonly
+      * @property {Number} length length of SceneGraph
+      */
     'length': { get: function() {return self.children.length;}},
   });
-  
+
   /**
-    *
-	*/
+   * Gets all elements in a SceneGraph with a certain name
+   * @function SceneGraph#byName
+   * @param {String} name name to search for
+   * @returns {Array} all children with name
+   */
   self.byName = function(name) {
     return self.children.filter(function(e) { return e.name == name; });
   }
   
   /**
-    *
-	*/
+   * Gets first element in a SceneGraph with a certain name
+   * @function SceneGraph#firstByName
+   * @param {String} name name to search for
+   * @returns {object} first child with name
+   */
   self.firstByName = function(name) {
     var temp = self.children.filter(function(e) { return e.name == name; });
     if (temp.length == 0) { //doesn't exist
@@ -176,8 +237,11 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+   * Gets last element in a SceneGraph with a certain name
+   * @function SceneGraph#lastByName
+   * @param {String} name name to search for
+   * @returns {object} last child with name
+   */
   self.lastByName = function(name) {
     var temp = self.filter(function(e) { return e.name == name; });
     if (temp.length == 0) { //doesn't exist
@@ -188,8 +252,11 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * removes the object at a certain index into the scene graph
+    * @function SceneGraph#removeIndex
+    * @param {Number} index the index to remove
+    * @returns {object} the removed object
+    */
   self.removeIndex = function(index) {
     var temp = self.children[index];
     self.children.splice(index, 1);
@@ -197,36 +264,49 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * removes the given object from the scene graph
+    * @function SceneGraph#removeIndex
+    * @param {object} e the object to remove
+    * @returns {object} the removed object
+    */
   self.remove = function(e) {
     return self.removeIndex(self.indexOf(e));
   }
   
   /**
-    *
-	*/
+    * removes all objects from the scene graph
+    * @function SceneGraph#removeAll
+    */
   self.removeAll = function() {
     self.children.splice(0, self.children.length);
   }
   
   /**
-    *
-	*/
+    * finds the index of an object in the scene graph
+    * @function SceneGraph#indexOf
+    * @returns {Number} the index of the object, -1 if not found
+    * @param {object} e the object to find
+    */
   self.indexOf = function(e) {
     return self.children.indexOf(e);
   }
   
   /**
-    *
-	*/
+    * Runs a foreach query on the scene graph. Proxy for SceneGraph.children.forEach(func)
+    * @function SceneGraph#forEach
+    * @param {function} func the function to run
+    */
   self.forEach = function(func) {
     self.children.forEach(func);
   }
   
   /**
-    *
-	*/
+    * Runs a foreach query on the scene graph until a child returns true.
+    * @function SceneGraph#forEachUntilFirstSuccess
+    * @param {function} func the function to run
+    * @param {Boolean} [deepCheck=false] if true, will expand all child scene graphs when running query
+    * @return {Boolean|object} object which returned true; if no objects return true, returns false
+    */
   self.forEachUntilFirstSuccess = function(func, deepCheck = false) {
     for (var i = 0; i < self.length; ++i) {
       if (deepCheck && self.children[i]._isAggregate) {
@@ -243,8 +323,11 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Runs a foreach query on the scene graph and returns the results.
+    * @function SceneGraph#forEachReturn
+    * @param {function} func the function to run
+    * @returns {object[]} outputs of forEach
+    */
   self.forEachReturn = function(func) {
     var output = new Array();
     for (var i = 0; i < self.length; ++i) {
@@ -254,8 +337,11 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Runs a foreach query on the scene graph in reverse.
+    * @function SceneGraph#forEachReverse
+    * @see SceneGraph#forEach
+    * @param {function} func the function to run
+    */
   self.forEachReverse = function(func) {
     for (var i = self.length - 1; i >= 0; --i) {
       func(self.children[i], i, self.children);
@@ -263,8 +349,13 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Runs a foreach query on the scene graph in reverse until a child returns true.
+    * @function SceneGraph#forEachReverseUntilFirstSuccess
+    * @see SceneGraph#forEachUntilFirstSuccess
+    * @param {function} func the function to run
+    * @param {Boolean} [deepCheck=false] if true, will expand all child scene graphs when running query
+    * @return {Boolean|object} object which returned true
+    */
   self.forEachReverseUntilFirstSuccess = function(func) {
     for (var i = self.length - 1; i >= 0; --i) {
       if (func(self.children[i], i, self.children)) {
@@ -274,8 +365,12 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Runs a foreach query on the scene graph in reverse and returns the results.
+    * @function SceneGraph#forEachReverseReturn
+    * @see SceneGraph#forEachReturn
+    * @param {function} func the function to run
+    * @returns {object[]} outputs of forEach
+    */
   self.forEachReverseReturn = function(func) {
     var output = new Array();
     for (var i = self.length - 1; i >= 0; --i) {
@@ -285,15 +380,20 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Filters the SceneGraph. Proxy for SceneGraph.children.filter
+    * @function SceneGraph#filter
+    * @param {function} func filter to use
+    * @returns {Array} filtered data
+    */
   self.filter = function(func) {
     return self.children.filter(func);
   }
   
   /**
-    *
-	*/
+    * Moves an object to the front of the SceneGraph
+    * @param {Number} index index to move
+    * @function SceneGraph#moveToFront
+    */
   self.moveToFront = function(index) {
     var temp = self.children[index];
     for (var i = index; i > 0; --i) {
@@ -303,8 +403,10 @@ function SceneGraph(name, doUpdate=true, doDraw=true, clickable=true, updateDire
   }
   
   /**
-    *
-	*/
+    * Moves an object to the back of the SceneGraph
+    * @param {Number} index index to move
+    * @function SceneGraph#moveToBack
+    */
   self.moveToBack = function(index) {
     var temp = self.children[index];
     for (var i = index; i < self.length - 1; ++i) {
--- objectPool.js
+++ objectPool.js
@@ -1,9 +1,95 @@
-const POOL_BEHAVIOR_ON_OVERFLOW = { RETURN_NULL:0, RETURN_NEW_AND_NOT_POOL:1, EXPAND_POOL:2 }
-const POOL_SELECTION = { FIRST:0, RANDOM:1 }
+/**
+ * Enum for behavior of ObjectPool when the number of currently spawned objects exceeds the pool size
+ * @readonly
+ * @enum {Number}
+ */
+const POOL_BEHAVIOR_ON_OVERFLOW = { 
+  /** The value for if the pool should just return null and not spawn any new objects */
+  RETURN_NULL:0,
+  /** The value for if the pool should spawn an object but not pool it */
+  RETURN_NEW_AND_NOT_POOL:1,
+  /** The value for if a pool should just expand its size to fit the new object being spawned */
+  EXPAND_POOL:2 
+}
+/**
+ * Enum for how objects are taken from the pool
+ * @readonly
+ * @enum {Number}
+ */
+const POOL_SELECTION = {
+  /** Select the first unallocated pool object */
+  FIRST:0,
+  /** Select a random unallocated pool object */
+  RANDOM:1
+}
 
+/**
+ * Returns a new GameObject
+ * @callback generatorCallback
+ * @returns GameObject
+ */
+
+/**
+ * Resets the properties of a GameObject to what they would be if it was a newly created object
+ * @callback resetFuncCallback
+ * @param {GameObject} obj object to reset
+ */
+ 
+/**
+ * Destructor for a GameObject
+ * @callback destroyFuncCallback
+ * @param {GameObject} obj object to destroy
+ */
+ 
+/**
+ * @typedef {Object} PooledInstance
+ * @property {GameObject} object instance of object
+ * @property {Boolean} isBeingUsed whether or not the pooled instance is currently allocated
+ * @property {Array} cRef pointer to list of spawned objects in ObjectPool
+ * @property {ObjectPool} object.pooledInstance pointer to ObjectPool that contains object
+ */
+
+/**
+ * An object pool that handles pooling automatically. Useful for particle systems
+ * @class ObjectPool
+ * @extends SceneGraph
+ * @param {String} name name of ObjectPool
+ * @param {generatorCallback} generator 
+ * @param {resetFuncCallback} resetFunc 
+ * @param {?destroyFuncCallback} destroyFunc 
+ * @param {Number} maxStore the maximum amount of objects that can be stored in the pool
+ * @param {POOL_BEHAVIOR_ON_OVERFLOW} behaviorOnOverflow how the pool should react when too many objects are spawned
+ * @param {POOL_SELECTION} [poolSelection=POOL_SELECTION.FIRST] how unallocated objects should be selected from the pool
+ * @param {Boolean} [doUpdate=true] whether or not to update the objects in the pool
+ * @param {Boolean} [doDraw=true] whether or not to draw the items in the pool
+ * @param {Boolean} [clickable=false] Whether or not the items in the pool are clickable
+ * @property {generatorCallback} objectConstructor the generator for new objects
+ * @property {resetFuncCallback} resetFunc the function to apply to all spawned objects
+ * @property {?destroyFuncCallback} destroyFunc the function to apply to all objects being destroyed, in case any cleanup is necessary
+ * @property {Number} maxStore the maximum pool size
+ * @property {Array} pooled the list of pooled objects, both allocated and unallocated
+ * @property {Array} children the allocated objects in the pool (i.e. only spawned ones)
+ * @property {POOL_SELECTION} select the selection method for allocation
+ * @property {POOL_BEHAVIOR_ON_OVERFLOW} bof the pool behavior on overflow
+ * @property {Boolean} _isAggregate=true true to indicate the object represents an aggregate of other objects
+ */
 var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, behaviorOnOverflow, poolSelection=POOL_SELECTION.FIRST, doUpdate=true, doDraw=true, clickable=false) {
   var self = this;
   
+  /**
+   * constructor
+   * @function ObjectPool#constructor
+   * @param {String} name name of ObjectPool
+   * @param {generatorCallback} generator 
+   * @param {resetFuncCallback} resetFunc 
+   * @param {?destroyFuncCallback} destroyFunc 
+   * @param {Number} maxStore the maximum amount of objects that can be stored in the pool
+   * @param {POOL_BEHAVIOR_ON_OVERFLOW} behaviorOnOverflow how the pool should react when too many objects are spawned
+   * @param {POOL_SELECTION} [poolSelection=POOL_SELECTION.FIRST] how unallocated objects should be selected from the pool
+   * @param {Boolean} [doUpdate=true] whether or not to update the objects in the pool
+   * @param {Boolean} [doDraw=true] whether or not to draw the items in the pool
+   * @param {Boolean} [clickable=false] Whether or not the items in the pool are clickable
+   */
   self.constructor = function(name, generator, resetFunc, destroyFunc, maxStore, behaviorOnOverflow, poolSelection, doUpdate=true, doDraw=true, clickable=false) {
     SceneGraph.call(self, name, doUpdate, doDraw, clickable);
     self.objectConstructor = generator;
@@ -15,7 +101,12 @@ var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, beh
     self.bof = behaviorOnOverflow;
     self._isAggregate = true;
   }
-  
+
+  /**
+   * Returns new instance of pooled object
+   * @function ObjectPool#newInstance
+   * @return {PooledInstance} new instance of allocated object
+   */
   self.newInstance = function() {
     var tmp = { object:self.objectConstructor(), isBeingUsed:true, cRef:self.children };
     tmp.object.pooledInstance = tmp;
@@ -23,6 +114,11 @@ var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, beh
     return tmp;
   }
   
+  /**
+   * Spawns a new object and adds it to the pool
+   * @function ObjectPool#spawn
+   * @returns {?PooledInstance} null if overflow and rules set to not allow overflow, else spawned object
+   */
   self.spawn = function() {
     if (self.pooled.length < maxStore) {
       var obj = self.newInstance();
@@ -56,6 +152,11 @@ var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, beh
     return available;
   }
   
+  /**
+   * Recycles a pooled instance from the pool
+   * @param {PooledInstance} pooledObject thing to recycle
+   * @function ObjectPool#recycle
+   */
   self.recycle = function(pooledObject) {
     self.remove(pooledObject.object);
     if (self.destroyFunc) {
@@ -64,6 +165,12 @@ var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, beh
     pooledObject.isBeingUsed = false;
   }
   
+  /**
+   * Spawns multiple objects
+   * @function ObjectPool#spawnSeveral
+   * @param {Number} n how many objects to spawn
+   * @returns {PooledInstance[]} list of spawned objects
+   */
   self.spawnSeveral = function(n) {
     var out = [];
     for (var i = 0; i < n; ++i) {
@@ -74,6 +181,12 @@ var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, beh
 
   self.constructor(name, generator, resetFunc, destroyFunc, maxStore, behaviorOnOverflow, poolSelection, doUpdate, doDraw, clickable);
   
+  /**
+   * Recycles/removes an object
+   * @function ObjectPool#remove
+   * @param {GameObject} e object to remove
+   * @returns {GameObject} removed object
+   */
   self.remove = function(e) {
     var f = self.pooled.find(function(p) {p.object === e});
     if (f) {
@@ -85,6 +198,10 @@ var ObjectPool = function(name, generator, resetFunc, destroyFunc, maxStore, beh
     return self.removeIndex(self.indexOf(e));
   }
   
+  /**
+   * Recycles all objects in pool
+   * @function ObjectPool#removeAll
+   */
   self.removeAll = function() {
     self.pooled.forEach(function(e) {e.isBeingUsed = false;});
     if (self.destroyFunc) {self.children.forEach(function(e) {self.destroyFunc(e)})};
